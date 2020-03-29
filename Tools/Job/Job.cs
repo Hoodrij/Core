651b2846793f56832adfb101048f796420bb9899
Slava Korzun <Hoodrij@gmail.com>
2020-03-29 22:59:30 +0300

    Task -> Job rename, major improvements

---
+++ Tools/Job/Job.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading.Tasks;
+
+namespace Core.Tools
+{
+	public abstract class Job
+    {
+        private Action<string> onError;
+
+        public TaskState State = TaskState.IDLE;
+
+        public Job()
+        {
+            Game.Models.Populate(this);
+        }
+
+        public async Task Run(Action<string> onError = null)
+        {
+            this.onError = onError;
+            State = TaskState.RUNNING;
+
+            await Run();
+            State = TaskState.SUCCESS;
+        }
+
+        protected abstract Task Run();
+
+        protected void ReportError(string errorMessage)
+        {
+            State = TaskState.ERROR;
+
+            onError?.Invoke(errorMessage);
+        }
+    }
+
+    public enum TaskState
+    {
+        IDLE,
+        RUNNING,
+        SUCCESS,
+        ERROR
+    }
+}
---
+++ Tools/Job/JobSequence.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Tools.ExtensionMethods;
+using Core.Tools.Observables;
+using UnityEngine;
+
+namespace Core.Tools
+{
+	public class JobSequence
+    {
+        public enum Mode
+        {
+            MultipleShot = 1,
+            OneByFrame = 2,
+        }
+        
+        public float Progress => 1 - (float) queue.Count / initialTasksCount;
+        
+        private readonly Signal<float> onProgressChanged = new Signal<float>();
+        private readonly Queue<Job> queue = new Queue<Job>();
+        private int initialTasksCount;
+        private Mode mode = Mode.MultipleShot;
+
+        public JobSequence Add(Job job)
+        {
+            queue.Enqueue(job);
+            return this;
+        }
+        
+        public JobSequence SetMode(Mode mode)
+        {
+            this.mode = mode;
+            return this;
+        }
+
+        public JobSequence OnProgress(Action<float> action)
+        {
+            onProgressChanged.Listen(action);
+            return this;
+        }
+        
+        public async Task<JobSequence> Run()
+        {
+            initialTasksCount = queue.Count;
+            await ExecutionRoutine();
+            
+            return this;
+        }
+
+        private async Task ExecutionRoutine()
+        {
+            while (queue.Count > 0)
+            {
+                await queue.Dequeue().Run();
+                onProgressChanged.Fire(Progress);
+
+                if (mode == Mode.OneByFrame)
+                    await new WaitForUpdate();
+            }
+        }
+    }
+}
