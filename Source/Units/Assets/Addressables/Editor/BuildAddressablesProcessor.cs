4a8f40d2a8ee16eaae4e1ad5293e788ada01abb2
Slava Korzun <Hoodrij@gmail.com>
2020-05-09 19:50:13 +0300

    BuildAddressablesProcessor

---
+++ Source/Units/Assets/Addressables/Editor/BuildAddressablesProcessor.cs
@@ -0,0 +1,40 @@
+#if ADDRESSABLES
+using UnityEditor;
+using UnityEditor.AddressableAssets;
+using UnityEditor.AddressableAssets.Settings;
+using UnityEngine;
+
+namespace Core.Units
+{
+    class BuildAddressablesProcessor
+    {
+        /// <summary>
+        /// Run a clean build before export.
+        /// </summary>
+        private static void PreExport()
+        {
+            Debug.Log("BuildAddressablesProcessor.PreExport start");
+            AddressableAssetSettings.CleanPlayerContent(AddressableAssetSettingsDefaultObject.Settings.ActivePlayerDataBuilder);
+            AddressableAssetSettings.BuildPlayerContent();
+            Debug.Log("BuildAddressablesProcessor.PreExport done");
+        }
+
+        [InitializeOnLoadMethod]
+        private static void Initialize()
+        {
+            BuildPlayerWindow.RegisterBuildPlayerHandler(BuildPlayerHandler);
+        }
+
+        private static void BuildPlayerHandler(BuildPlayerOptions options)
+        {
+            if (EditorUtility.DisplayDialog("Build with Addressables",
+                "Do you want to build a clean addressables before export?",
+                "Build with Addressables", "Skip"))
+            {
+                PreExport();
+            }
+            BuildPlayerWindow.DefaultBuildMethods.BuildPlayer(options);
+        }
+    }
+}
+#endif
