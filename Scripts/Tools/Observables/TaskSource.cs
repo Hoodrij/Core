2834cf54dbbe35a3538506ee6f3724e1a59e5d83
Slava Korzun <Hoodrij@gmail.com>
2021-02-10 21:16:06 +0200

    + TaskSource

---
+++ Scripts/Tools/Observables/TaskSource.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Core.Tools.Observables
+{
+    public class TaskSource<T> : CustomYieldInstruction
+    {
+        private bool isCompleted;
+        private T result;
+        
+        public void SetResult(T t)
+        {
+            result = t;
+            isCompleted = true;
+        }
+        
+        public static implicit operator T(TaskSource<T> taskSource) => taskSource.result;
+
+        public override bool keepWaiting => !isCompleted;
+    }
+}
