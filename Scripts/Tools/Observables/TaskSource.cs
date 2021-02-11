048e9f7454f1edcc794764a533e6243aa754053c
Slava Korzun <Hoodrij@gmail.com>
2021-02-11 09:31:16 +0200

    TaskSource rework + fix

--- Scripts/Tools/Observables/TaskSource.cs
+++ Scripts/Tools/Observables/TaskSource.cs
@@ -1,20 +1,45 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Core.Tools.Observables
 {
-    public class TaskSource<T> : CustomYieldInstruction
+    public static class TaskSourceEx
     {
-        private bool isCompleted;
-        private T result;
-        
-        public void SetResult(T t)
+        public static TaskSourceAwaiter<T> GetAwaiter<T>(this TaskSource<T> t) => new TaskSourceAwaiter<T>(t);
+    }
+
+    public class TaskSourceAwaiter<T> : INotifyCompletion
+    {
+        private TaskSource<T> taskSource;
+            
+        public TaskSourceAwaiter(TaskSource<T> taskSource)
         {
-            result = t;
-            isCompleted = true;
+            this.taskSource = taskSource;
         }
         
-        public static implicit operator T(TaskSource<T> taskSource) => taskSource.result;
+        public bool IsCompleted => taskSource.IsCompleted;
+        public T GetResult() => taskSource.Result;
 
-        public override bool keepWaiting => !isCompleted;
+        public void OnCompleted(Action continuation)
+        {
+            taskSource.OnCompleted += continuation;
+        }
+    }
+    
+    public class TaskSource<T>
+    {
+        internal bool IsCompleted;
+        internal T Result;
+        internal event Action OnCompleted;
+        
+        public void Complete(T t)
+        {
+            Result = t;
+            IsCompleted = true;
+            OnCompleted?.Invoke();
+            OnCompleted = null;
+        }
     }
 }
