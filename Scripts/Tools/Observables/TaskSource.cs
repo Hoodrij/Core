70be139543050765cf27338fe0bd76f1ae59a886
Slava Korzun <Hoodrij@gmail.com>
2021-02-11 17:53:58 +0200

    Model2 test

--- Scripts/Tools/Observables/TaskSource.cs
+++ Scripts/Tools/Observables/TaskSource.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
-using UnityEngine;
 
 namespace Core.Tools.Observables
 {
---
+++ Scripts/Units/Model2/Model2.cs
@@ -0,0 +1,17 @@
+﻿using Core.Tools.Observables;
+
+namespace Core.Units.Model2
+{
+    public class Model2<T>
+    {
+        private T t;
+        private Signal<T> signal = new Signal<T>();
+
+        public void Set(T t)
+        {
+            this.t = t;
+        }
+        
+        public static implicit operator T(Model2<T> model) => model.t;
+    }
+}
