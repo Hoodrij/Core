d438bfb4a5ac44d4394181b08bf2c5c2c97362ef
Slava Korzun <Hoodrij@gmail.com>
2021-02-12 23:29:36 +0200

    TypeEx

--- Scripts/Tools/ExtensionMethods/TypeEx.cs
+++ Scripts/Tools/ExtensionMethods/TypeEx.cs
@@ -97,5 +97,24 @@ namespace Core.Tools.ExtensionMethods
                 foreach (MethodInfo methodInfo in type.BaseType.GetAllMethodsWithAttribute<T>(recursive))
                     yield return methodInfo;
         }
+        
+        public static bool IsAssignableToGenericType(this Type givenType, Type genericType)
+        {
+            var interfaceTypes = givenType.GetInterfaces();
+
+            foreach (var it in interfaceTypes)
+            {
+                if (it.IsGenericType && it.GetGenericTypeDefinition() == genericType)
+                    return true;
+            }
+
+            if (givenType.IsGenericType && givenType.GetGenericTypeDefinition() == genericType)
+                return true;
+
+            Type baseType = givenType.BaseType;
+            if (baseType == null) return false;
+
+            return IsAssignableToGenericType(baseType, genericType);
+        }
     }
 }
