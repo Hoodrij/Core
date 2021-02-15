aa9a166e43b0efd04e2ced399d12980e5edd3b54
Slava Korzun <Hoodrij@gmail.com>
2021-02-15 09:54:21 +0200

    ShowPopupAndWaitForClose Yield

---
+++ Scripts/Tools/YieldInstructions/ShowPopupAndWaitForClose.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Core.Tools.Observables;
+using Core.Ui;
+using UnityEngine;
+
+namespace Core.Tools.Yield
+{
+    public class ShowPopupAndWaitForClose : CustomYieldInstruction
+    {
+        private bool isClosed; 
+            
+        public ShowPopupAndWaitForClose(Func<Task<UIView>> openFunc)
+        {
+            Open(openFunc);
+        }
+
+        private async void Open(Func<Task<UIView>> openFunc)
+        {
+            UIView view = await openFunc();
+            await view.closeEvent;
+            isClosed = true;
+        }
+
+        public override bool keepWaiting => !isClosed;
+    }
+}
