0f938ca432eda55d8124e8b3a800e61545d2610c
Slava Korzun <Hoodrij@gmail.com>
2021-02-19 15:34:12 +0200

    IntEx Remap

--- Scripts/Tools/ExtensionMethods/IntEx.cs
+++ Scripts/Tools/ExtensionMethods/IntEx.cs
@@ -11,5 +11,15 @@
         {
             return i == 1;
         }
+        
+        public static int Remap(this int value, int from1, int to1, int from2, int to2)
+        {
+            return (int) (((float) (value - from1)) / (to1 - from1) * (to2 - from2) + from2);
+        }
+
+        public static int Remap01(this int value, int from1, int to1)
+        {
+            return (value - from1) / (to1 - from1);
+        }
     }
 }
