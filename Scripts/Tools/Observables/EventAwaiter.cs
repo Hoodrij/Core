80255d1dd3e0f45ce4cbd63763924e9df64d2690
Slava Korzun <Hoodrij@gmail.com>
2021-03-19 11:16:35 +0200

    EventEx.ToTask

--- Scripts/Tools/Observables/EventAwaiter.cs
+++ Scripts/Tools/Observables/EventAwaiter.cs
@@ -58,10 +58,4 @@ namespace Core.Tools.Observables
             continuation();
         }
     }
-
-    public static class EventEx
-    {
-        public static EventAwaiter GetAwaiter(this Event e) => new EventAwaiter(e);
-        public static EventAwaiter<T> GetAwaiter<T>(this Event<T> e) => new EventAwaiter<T>(e);
-    }
 }
---
+++ Scripts/Tools/Observables/EventEx.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+namespace Core.Tools.Observables
+{
+    public static class EventEx
+    {
+        public static Task ToTask(this Event @event)
+        {
+            return Task.Run(async () => await @event);
+        }
+        
+        public static EventAwaiter GetAwaiter(this Event e)
+        {
+            return new EventAwaiter(e);
+        }
+
+        public static EventAwaiter<T> GetAwaiter<T>(this Event<T> e)
+        {
+            return new EventAwaiter<T>(e);
+        }
+    }
+}
