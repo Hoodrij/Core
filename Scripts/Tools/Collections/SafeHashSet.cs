5f49301b77610995746408f35178a2c3588453c9
Slava Korzun <Hoodrij@gmail.com>
2021-03-27 00:26:21 +0200

    SafeHashSet

---
+++ Scripts/Tools/Collections/SafeHashSet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Tools.ExtensionMethods;
+using UnityAsync;
+
+namespace Core.Tools.Collections
+{
+    public class SafeHashSet<T>
+    {
+        private readonly HashSet<T> collection = new HashSet<T>();
+        private bool isIterating;
+
+        public void ForEach(Action<T> action)
+        {
+            isIterating = true;
+            collection.ForEach(action);
+            isIterating = false;
+        }
+        
+        public async void Add(T item)
+        {
+            await WaitUnlock();
+            collection.Add(item);
+        }
+
+        public async void RemoveWhere(Predicate<T> match)
+        {
+            await WaitUnlock();
+            collection.RemoveWhere(match);
+        }
+
+        public async void Clear()
+        {
+            await WaitUnlock();
+            collection.Clear();
+        }
+
+        private async Task WaitUnlock()
+        {
+            if (isIterating)
+                await Wait.While(() => isIterating);
+        }
+    }
+}
