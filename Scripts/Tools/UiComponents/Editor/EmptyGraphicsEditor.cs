f8a84e31352a4d08d8fd1cb96b2503231d7ed853
Slava Korzun <Hoodrij@gmail.com>
2021-05-23 20:05:56 +0300

    EmptyGraphics

---
+++ Scripts/Tools/UiComponents/Editor/EmptyGraphicsEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEditor.UI;
+
+namespace Core.Tools.UiComponents.Editor
+{
+    [CanEditMultipleObjects, CustomEditor(typeof(EmptyGraphics), false)]
+    public class NonDrawingGraphicEditor : GraphicEditor
+    {
+        public override void OnInspectorGUI ()
+        {
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(m_Script);
+            // skipping AppearanceControlsGUI
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
